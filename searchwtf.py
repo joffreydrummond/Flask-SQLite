0b3e1362078fa5838a0805282778167501bffd95
Joffrey <jaydru@gmail.com>
2025-11-20 09:25:21 -0700

    Amended ByPublisherIdWTF to access BooksDB and use getpublisher method and ByTitleWTF

--- searchwtf.py
+++ searchwtf.py
@@ -15,9 +15,11 @@ class ByAuthorIdWTF(FlaskForm):
 
 
 class ByPublisherIdWTF(FlaskForm):
-    pass
+    mydb = BooksDB()
+    publishers = mydb.getpublishers()
+    publisher_choice = SelectField("PublisherChoice", choices=publishers)
 
 class ByTitleWTF(FlaskForm):
     # This will also need a stringfield for the words the user types
-    pass
+    title_words = StringField("Title Contains", validators=[DataRequired()])
 
